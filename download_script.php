e6f9b6c482d3ec03af9059060c4f8bb81db2d1af
stopopol <christoph.wohner@umweltbundesamt.at>
2017-09-05 15:11:02 +0200

    Add files via upload

---
+++ download_script.php
@@ -0,0 +1,30 @@
+<?php
+
+//
+// Download script for ISO 19139 metadata records from DEIMS-SDR
+//
+
+$url = 'https://data.lter-europe.net/deims/iso/harvest_list';
+echo $url."\n";
+$xml = simplexml_load_file($url) or die("feed not loading");
+
+$arr = json_decode(json_encode($xml), true);
+$zwischen_var = $arr["dataset"]; 
+
+$files = glob(__DIR__ . "/iso19139_files/*"); // get all file names
+foreach($files as $file){ // iterate files
+  if(is_file($file))
+    unlink($file); // delete file
+}
+
+for ($x = 0; $x < count($zwischen_var); $x++) {
+//for ($x = 0; $x < 3; $x++) {
+    $temp_record = $zwischen_var[$x];
+	
+	// get current directory
+	$file_name = __DIR__ . "/iso19139_files/".$temp_record["UUID"].".xml";	
+	file_put_contents($file_name, fopen($temp_record["path"], 'r'));
+	
+}
+
+?>
