e08a8ee385ba799c53b898e3f621b444f63b6b78
stopopol <christoph.wohner@umweltbundesamt.at>
2017-12-04 10:22:29 +0100

    Create deims_all_sites.sql

---
+++ sql/deims_all_sites.sql
@@ -0,0 +1,16 @@
+CREATE VIEW deims_all_sites AS
+
+SELECT 
+name.`field_site_sitelong_value` AS name,
+basetable.`uuid`,
+basetable.`nid`,
+coordinates.`field_coordinates_lat`, 
+coordinates.`field_coordinates_lon`,
+point(`coordinates`.`field_coordinates_lon`,`coordinates`.`field_coordinates_lat`) AS `geom` 
+
+FROM `node` basetable
+INNER JOIN `field_data_field_coordinates` coordinates
+ON coordinates.`entity_id` = basetable.`nid`
+INNER JOIN `field_data_field_site_sitelong` name
+ON name.`entity_id` = basetable.`nid`
+WHERE basetable.`status` = 1;
