df1fabd9c1aa511456be6cf2f2103882ed9e9050
stopopol <christoph.wohner@umweltbundesamt.at>
2018-02-07 16:04:28 +0100

    Update lter_cwn.sql

--- sql/lter_cwn.sql
+++ sql/lter_cwn.sql
@@ -1,12 +1,13 @@
-CREATE VIEW lter_cwn_sites AS 
+CREATE OR REPLACE VIEW lter_cwn_sites AS 
 
 SELECT 
 `name`.`field_site_sitelong_value` AS `name`,
 `basetable`.`uuid` AS `uuid`,
 `basetable`.`nid` AS `nid`,
-classi.`field_site_eu_classification_value` AS classification, 
+classi.`field_site_eu_classification_value` AS classification,
 `coordinates`.`field_coordinates_lat` AS `field_coordinates_lat`,
 `coordinates`.`field_coordinates_lon` AS `field_coordinates_lon`,
+msl.`field_elevation_average_value`,
 point(`coordinates`.`field_coordinates_lon`,`coordinates`.`field_coordinates_lat`) AS `geom` 
 
 from (((`node` `basetable` join `field_data_field_coordinates` `coordinates` on((`coordinates`.`entity_id` = `basetable`.`nid`))) 
@@ -16,5 +17,8 @@ join `field_data_field_networks_term_ref` `project_networks` on((`project_networ
 
 INNER JOIN  `field_data_field_site_eu_classification` classi
 ON classi.`entity_id` = basetable.`nid`
+
+INNER JOIN `field_data_field_elevation_average` msl
+ON msl.`entity_id` = basetable.`nid`
 
 where (`project_networks`.`field_networks_term_ref_tid` = 53155);
