2d0f926fa2128023ebf87ae3497cd7a72383e8e1
stopopol <christoph.wohner@umweltbundesamt.at>
2018-02-07 16:29:45 +0100

    Create ecopotential.sql

---
+++ sql/ecopotential.sql
@@ -0,0 +1,24 @@
+CREATE OR REPLACE VIEW `ecopotential_sites` AS 
+SELECT
+
+`NAME`.`field_site_sitelong_value`                                                 AS `NAME`,
+`basetable`.`uuid`                                                                 AS `uuid`,
+`basetable`.`nid`                                                                  AS `nid`,
+`coordinates`.`field_coordinates_lat`                                              AS `field_coordinates_lat`,
+`coordinates`.`field_coordinates_lon`                                              AS `field_coordinates_lon`,
+msl.`field_elevation_average_value`,
+point(`coordinates`.`field_coordinates_lon`,`coordinates`.`field_coordinates_lat`) AS `geom`
+
+FROM   `node` `basetable` 
+JOIN   `field_data_field_coordinates` `coordinates` 
+ON `coordinates`.`entity_id` = `basetable`.`nid`
+
+JOIN   `field_data_field_site_sitelong` `NAME` 
+ON    `NAME`.`entity_id` = `basetable`.`nid`
+
+INNER JOIN `field_data_field_elevation_average` msl
+ON msl.`entity_id` = basetable.`nid`
+
+JOIN   `field_data_field_networks_term_ref` `project_networks` 
+ON     `project_networks`.`entity_id` = `basetable`.`nid`
+WHERE `project_networks`.`field_networks_term_ref_tid` = 52260
