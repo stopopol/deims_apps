119562e10a19585acca3f376e253125d5d922334
stopopol <christoph.wohner@umweltbundesamt.at>
2018-02-07 16:33:32 +0100

    Create teabag_all.sql

---
+++ sql/teabag_all.sql
@@ -0,0 +1,31 @@
+CREATE OR REPLACE 
+
+VIEW `teabag_all` AS 
+
+SELECT 
+
+`NAME`.`field_site_sitelong_value`                                                 AS `name`,
+`basetable`.`nid`                                                                  AS `nid`,
+`uuid`.`field_uuid_value`                                                          AS `uuid`,
+`coordinates`.`field_coordinates_lat`                                              AS `field_coordinates_lat`,
+`coordinates`.`field_coordinates_lon`                                              AS `field_coordinates_lon`,
+msl.`field_elevation_average_value`,
+point(`coordinates`.`field_coordinates_lon`,`coordinates`.`field_coordinates_lat`) AS `geom`
+
+FROM   `node` `basetable` 
+JOIN   `field_data_field_coordinates` `coordinates` 
+ON    `coordinates`.`entity_id` = `basetable`.`nid`
+ 
+JOIN   `field_data_field_site_sitelong` `NAME` 
+ON    `NAME`.`entity_id` = `basetable`.`nid`
+ 
+JOIN   `field_data_field_uuid` `uuid` 
+ON     `uuid`.`entity_id` = `basetable`.`nid`
+
+INNER JOIN `field_data_field_elevation_average` msl
+ON msl.`entity_id` = basetable.`nid`
+
+JOIN   `field_data_field_networks_term_ref` `networks` 
+ON    `networks`.`entity_id` = `basetable`.`nid` 
+WHERE   `networks`.`field_networks_term_ref_tid` = 52727
+       AND    `basetable`.`status` = 1
