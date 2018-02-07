d3b88b976830e0da681580a88712b4e6e3d4d21e
stopopol <christoph.wohner@umweltbundesamt.at>
2018-02-07 15:49:35 +0100

    Update deims_all_sites.sql

--- sql/deims_all_sites.sql
+++ sql/deims_all_sites.sql
@@ -6,6 +6,8 @@ basetable.`uuid`,
 basetable.`nid`,
 coordinates.`field_coordinates_lat`, 
 coordinates.`field_coordinates_lon`,
+msl.`field_elevation_average_value`,
+
 point(`coordinates`.`field_coordinates_lon`,`coordinates`.`field_coordinates_lat`) AS `geom` 
 
 FROM `node` basetable
@@ -14,5 +16,8 @@ ON coordinates.`entity_id` = basetable.`nid`
 
 INNER JOIN `field_data_field_site_sitelong` name
 ON name.`entity_id` = basetable.`nid`
+
+INNER JOIN `field_data_field_elevation_average` msl
+ON msl.`entity_id` = basetable.`nid`
 
 WHERE basetable.`status` = 1 and coordinates.`field_coordinates_lat` 
