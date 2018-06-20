edfaf2177bad33c5894b629679d59b0fd391ae02
stopopol <christoph.wohner@umweltbundesamt.at>
2018-06-20 11:46:59 +0200

    Create sites_bboxes.sql

---
+++ sql/sites_bboxes.sql
@@ -0,0 +1,21 @@
+CREATE OR REPLACE VIEW deims_sites_bboxes AS
+
+SELECT 
+name.`field_site_sitelong_value` AS name,
+basetable.`uuid`,
+basetable.`nid`,
+coordinates.`field_geo_bounding_box_geom`, 
+msl.`field_elevation_average_value`
+
+
+FROM `node` basetable
+INNER JOIN `field_data_field_geo_bounding_box` coordinates
+ON coordinates.`entity_id` = basetable.`nid`
+
+INNER JOIN `field_data_field_site_sitelong` name
+ON name.`entity_id` = basetable.`nid`
+
+LEFT JOIN `field_data_field_elevation_average` msl
+ON msl.`entity_id` = basetable.`nid`
+
+WHERE basetable.`status` = 1 
