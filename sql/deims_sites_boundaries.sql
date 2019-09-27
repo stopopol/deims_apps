e3509cb39fa0d9d9debad4fa3cb3aef2091375b1
stopopol <christoph.wohner@umweltbundesamt.at>
2019-09-27 13:34:21 +0200

    Create deims_sites_boundaries.sql

---
+++ sql/deims_sites_boundaries.sql
@@ -0,0 +1,18 @@
+CREATE OR REPLACE VIEW deims_sites_boundaries AS
+
+SELECT 
+name.`field_site_sitelong_value` AS name,
+CONCAT('https://deims.org/', basetable.`uuid`) AS deimsid,
+basetable.`nid`,
+
+ST_MPOLYFROMWKB (`field_geo_bounding_box_geom`) AS geom
+
+
+FROM `node` basetable
+INNER JOIN `field_data_field_geo_bounding_box` coordinates
+ON coordinates.`entity_id` = basetable.`nid`
+
+INNER JOIN `field_data_field_site_sitelong` AS name
+ON name.`entity_id` = basetable.`nid`
+
+WHERE `field_geo_bounding_box_geo_type` = 'polygon' OR `field_geo_bounding_box_geo_type` = 'multipolygon'
