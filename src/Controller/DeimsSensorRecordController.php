f9b9aab7813c4b5524a5744180ea480938aa330b
stopopol <christoph.wohner@umweltbundesamt.at>
2019-10-21 17:30:17 +0200

    Add files via upload

--- src/Controller/DeimsSensorRecordController.php
+++ src/Controller/DeimsSensorRecordController.php
@@ -56,7 +56,23 @@ class DeimsSensorRecordController extends ControllerBase {
 
 		$sensor_information['general']['name'] = $node->get('title')->value;
 		$sensor_information['general']['uuid'] = $node->get('uuid')->value;
+		$sensor_information['general']['relatedSite'] = $DeimsFieldController->parseEntityReferenceField($node->get('field_related_site'));
+		$sensor_information['general']['contact'] = $DeimsFieldController->parseEntityReferenceField($node->get('field_contact'));
+		$sensor_information['general']['abstract'] = $node->get('field_abstract')->value;
+		$sensor_information['general']['dateRange']['from'] = (!is_null($node->get('field_date_range')->value)) ? ($node->get('field_date_range')->value) : null;
+		$sensor_information['general']['dateRange']['to'] = (!is_null($node->get('field_date_range')->end_value)) ? ($node->get('field_date_range')->end_value) : null;
+		$sensor_information['general']['keywords']= $DeimsFieldController->parseEntityReferenceField($node->get('field_keywords'));
 
+		$sensor_information['geographic']['coordinates'] = $node->get('field_coordinates')->value;
+		$sensor_information['geographic']['trajectory'] = $node->get('field_boundaries')->value;
+		$sensor_information['geographic']['elevation'] = (!is_null($node->get('field_elevation_avg')->value)) ? floatval($node->get('field_elevation_avg')->value)  : null;
+
+		$sensor_information['observation']['sensorType'] = $DeimsFieldController->parseEntityReferenceField($node->get('field_sensor_type'), true);
+		$sensor_information['observation']['resultAcquisitionSource'] = $DeimsFieldController->parseTextListField($node, $fieldname = 'field_result_acquisition_source', $single_value_field=true);	
+		$sensor_information['observation']['observedProperty'] = $DeimsFieldController->parseEntityReferenceField($node->get('field_observation'));
+		/*
+			field_observation
+		*/
 		
 		return $sensor_information;
 		
