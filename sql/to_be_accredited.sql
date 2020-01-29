c0aa7268bfde906424ebe9fbcff37c45349f2c92
stopopol <christoph.wohner@umweltbundesamt.at>
2020-01-29 18:08:24 +0100

    Update to_be_accredited.sql

--- sql/to_be_accredited.sql
+++ sql/to_be_accredited.sql
@@ -2,7 +2,6 @@ CREATE OR REPLACE VIEW to_be_accredited AS
 
 -- get all sites and their completeness and print affiliation
 
-
 SELECT DISTINCT
 name.`field_name_value` AS name,
 CONCAT('https://deims.org/', basetable.`uuid`) as deimsid,
@@ -107,3 +106,24 @@ WHERE verified.`field_network_verified_value` = 1
 
 -- filter by lter networks
 -- AND network.`field_network_target_id` IN (219, 255, 219, 255, 232, 244, 215, 229, 216, 249, 236, 220, 212, 211, 245, 223, 10463, 10723, 230, 231, 233, 235, 237, 238, 240, 241, 242, 243, 247, 248, 250, 217, 218, 221, 222, 226, 227, 228, 209, 208, 239, 210, 232, 244, 215, 229, 216, 249, 236, 220, 212, 211, 245, 223, 10463, 10723, 230, 231, 233, 235, 237, 238, 240, 241, 242, 243, 247, 248, 250, 217, 218, 221, 222, 226, 227, 228, 209, 208, 239, 210)
+
+-- exclude all IDs that have ilter reference (nid 12833)
+
+AND  basetable.`nid` IN (
+
+	SELECT basetable.nid
+	FROM `node` basetable
+
+	LEFT JOIN `node__field_affiliation` as affiliation
+	ON affiliation.`entity_id` = basetable.`nid`
+
+	LEFT JOIN `paragraph__field_network` as network
+	ON network.`entity_id` = affiliation.`field_affiliation_target_id`
+
+	WHERE basetable.type = 'site'
+
+	group by basetable.nid
+	having sum(case when network.`field_network_target_id` = 12833 then 1 else 0 end) = 0
+
+) 
+
