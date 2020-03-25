cbe2c7c1d0e502c585f41a139040f5750aa0bc2b
stopopol <christoph.wohner@umweltbundesamt.at>
2020-03-25 09:31:50 +0100

    Create download_metadata.sh

---
+++ download_metadata.sh
@@ -0,0 +1,12 @@
+wget  -O  deims_sites.json https://deims.org/api/sites
+for code in `jq .[].id.suffix deims_sites.json | sed "s/\"//g"`; do  echo $code; wget "https://pydeims.dev.klimeto.com/gmd/site?uuid=${code}"; done
+
+wget  -O  deims_datasets.json https://deims.org/api/datasets
+for code in `jq .[].id.suffix deims_datasets.json | sed "s/\"//g"`; do  echo $code; wget "https://pydeims.dev.klimeto.com/gmd/dataset?uuid=${code}"; done
+
+wget  -O  deims_activities.json https://deims.org/api/activities
+for code in `jq .[].id.suffix deims_activities.json | sed "s/\"//g"`; do  echo $code; wget "https://pydeims.dev.klimeto.com/gmd/activity?uuid=${code}"; done
+
+rm deims_sites.json
+rm deims_datasets.json
+rm deims_activities.json
