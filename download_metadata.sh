debd290e430beee23a6a793545023f02f07d79f2
stopopol <christoph.wohner@umweltbundesamt.at>
2020-03-25 09:44:41 +0100

    Update download_metadata.sh

--- download_metadata.sh
+++ download_metadata.sh
@@ -1,8 +1,8 @@
 wget  -O  deims_sites.json https://deims.org/api/sites
-for code in `jq .[].id.suffix deims_sites.json | sed "s/\"//g"`; do  echo $code; wget "https://pydeims.dev.klimeto.com/gmd/site?uuid=${code}"; done
+for code in `jq .[].id.suffix deims_sites.json | sed "s/\"//g"`; do  echo $code; wget "https://pydeims.dev.klimeto.com/gmd/site?uuid=${code}" -O ${code}.xml; done
 
 wget  -O  deims_datasets.json https://deims.org/api/datasets
-for code in `jq .[].id.suffix deims_datasets.json | sed "s/\"//g"`; do  echo $code; wget "https://pydeims.dev.klimeto.com/gmd/dataset?uuid=${code}"; done
+for code in `jq .[].id.suffix deims_datasets.json | sed "s/\"//g"`; do  echo $code; wget "https://pydeims.dev.klimeto.com/gmd/dataset?uuid=${code}" -O ${code}.xml; done
 
 wget  -O  deims_activities.json https://deims.org/api/activities
 for code in `jq .[].id.suffix deims_activities.json | sed "s/\"//g"`; do  echo $code; wget "https://pydeims.dev.klimeto.com/gmd/activity?uuid=${code}" -O ${code}.xml; done
