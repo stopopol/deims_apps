bd0658df33968e9bee8b204e83c23f9df58a01c5
stopopol <christoph.wohner@umweltbundesamt.at>
2021-11-17 16:20:37 +0100

    Update locations_all.sql

--- sql/locations_all.sql
+++ sql/locations_all.sql
@@ -1,7 +1,8 @@
 SELECT 
 name.`title` AS name,
 basetable.`uuid` as uuid,
-ST_GeomFromText(boundaries.field_boundaries_value) as geom
+ST_GeomFromText(boundaries.field_boundaries_value) as geom,
+location_type_label.`name`
 
 FROM `node` basetable
 
@@ -10,3 +11,9 @@ ON name.`nid` = basetable.`nid`
 
 INNER JOIN `node__field_boundaries` boundaries
 ON boundaries.`entity_id` = basetable.`nid`
+
+LEFT JOIN `node__field_location_type` location_type
+ON location_type.`entity_id` = basetable.`nid`
+
+LEFT JOIN `taxonomy_term_field_data` location_type_label
+ON location_type.`field_location_type_target_id` = location_type_label.`name`
