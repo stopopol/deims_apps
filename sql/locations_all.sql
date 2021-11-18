fdd0ab60d58a73aa8ebecb565cd6d59446281771
stopopol <christoph.wohner@umweltbundesamt.at>
2021-11-18 11:41:13 +0100

    Update locations_all.sql

--- sql/locations_all.sql
+++ sql/locations_all.sql
@@ -9,13 +9,13 @@ location_type_label.`name` as type
 FROM `node` basetable
 
 INNER JOIN `node_field_revision` as name
-ON name.`nid` = basetable.`nid`
+ON name.`nid` = basetable.`nid` 
 
 INNER JOIN `node__field_boundaries` boundaries
 ON boundaries.`entity_id` = basetable.`nid`
 
 LEFT JOIN `node__field_location_type` location_type
-ON location_type.`entity_id` = basetable.`nid`
+ON location_type.`entity_id` = basetable.`nid` WHERE basetable.`type` = 'observation_location'
 
 LEFT JOIN `taxonomy_term_field_data` location_type_label
 ON location_type.`field_location_type_target_id` = location_type_label.`name`
