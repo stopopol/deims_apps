38b1510d4fa24b3a98292961a9aebdb829d9b306
stopopol <christoph.wohner@umweltbundesamt.at>
2025-08-11 11:19:38 +0200

    Update pycsw-loader.sh

--- pycsw-loader.sh
+++ pycsw-loader.sh
@@ -14,6 +14,9 @@ curl -s https://deims.org/api/sites | for code in $(jq .[].id.suffix | sed "s/\"
 curl -s https://deims.org/api/datasets | for code in $(jq .[].id.suffix | sed "s/\"//g"); do curl -s "https://deims.org/data/iso19139/gmd/dataset?uuid=${code}" -o "${dest}/${code}"_dataset.xml; done
 curl -s https://deims.org/api/activities | for code in $(jq .[].id.suffix | sed "s/\"//g"); do curl -s "https://deims.org/data/iso19139/gmd/activity?uuid=${code}" -o "${dest}/${code}"_activity.xml; done
 
+# for the updated iso19139 site records
+# curl -s https://deims.org/api/sites | for code in $(jq .[].id.suffix | sed "s/\"//g"); do curl -s "https://deims.org/api/sites/${code}?format=iso19139" -o "${dest}/${code}"_site.xml; done
+
 # check firewall issues
 # bash script might have to be adjusted for unix using $ dos2unix pycsw.sh
 
