f45bbf71ec782d3636e958c6732b78d4dd8424bc
stopopol <christoph.wohner@umweltbundesamt.at>
2026-08-24 15:18:25 +0200

    Remove XML download commands from pycsw-loader.sh
    Removed download commands for XML files from DEIMS API.

--- pycsw-loader.sh
+++ pycsw-loader.sh
@@ -9,11 +9,6 @@ dest="/home/ubuntu/cswdatabase/iso19139_files"
 
 rm -Rf "${dest}" && mkdir -p "${dest}"
 
-# download xml from deims api
-curl -s https://deims.org/api/sites | for code in $(jq .[].id.suffix | sed "s/\"//g"); do curl -s "https://deims.org/data/iso19139/gmd/site?uuid=${code}" -o "${dest}/${code}"_site.xml; done
-curl -s https://deims.org/api/datasets | for code in $(jq .[].id.suffix | sed "s/\"//g"); do curl -s "https://deims.org/data/iso19139/gmd/dataset?uuid=${code}" -o "${dest}/${code}"_dataset.xml; done
-curl -s https://deims.org/api/activities | for code in $(jq .[].id.suffix | sed "s/\"//g"); do curl -s "https://deims.org/data/iso19139/gmd/activity?uuid=${code}" -o "${dest}/${code}"_activity.xml; done
-
 # for the updated iso19139 site records
 # curl -s https://deims.org/api/sites | for code in $(jq .[].id.suffix | sed "s/\"//g"); do curl -s "https://deims.org/api/sites/${code}?format=iso19139" -o "${dest}/${code}"_site.xml; done
 
