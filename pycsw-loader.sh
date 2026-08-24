d449fbf39a4709e80b480e635bc34c18e72c2469
stopopol <christoph.wohner@umweltbundesamt.at>
2026-08-24 15:19:04 +0200

    Enable downloading of iso19139 site records
    Updated script to download iso19139 site records from API.

--- pycsw-loader.sh
+++ pycsw-loader.sh
@@ -9,8 +9,8 @@ dest="/home/ubuntu/cswdatabase/iso19139_files"
 
 rm -Rf "${dest}" && mkdir -p "${dest}"
 
-# for the updated iso19139 site records
-# curl -s https://deims.org/api/sites | for code in $(jq .[].id.suffix | sed "s/\"//g"); do curl -s "https://deims.org/api/sites/${code}?format=iso19139" -o "${dest}/${code}"_site.xml; done
+# download iso19139 site records from api
+curl -s https://deims.org/api/sites | for code in $(jq .[].id.suffix | sed "s/\"//g"); do curl -s "https://deims.org/api/sites/${code}?format=iso19139" -o "${dest}/${code}"_site.xml; done
 
 # check firewall issues
 # bash script might have to be adjusted for unix using $ dos2unix pycsw.sh
