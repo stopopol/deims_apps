420b6099a829e56960eca8cd4f592309f600c39e
stopopol <christoph.wohner@umweltbundesamt.at>
2026-08-24 15:39:13 +0200

    Refactor pycsw-loader.sh for better error handling
    Updated the pycsw-loader.sh script to improve error handling and streamline file processing.

--- pycsw-loader.sh
+++ pycsw-loader.sh
@@ -1,30 +1,43 @@
-#!/usr/bin/env bash
 
-set -o errexit
-set -o pipefail
-set -o xtrace
+# This script populates an existing pyCSW docker container
+# use this cronjob to call it periodically
+# 0 1 * * 0 /opt/elter-scripts/pycsw-loader.sh > /home/ubuntu/cswdatabase/iso19139.log
 
 # set location of xml files, this is configured in pycsw.cfg so needs to match
 dest="/home/ubuntu/cswdatabase/iso19139_files"
 
-rm -Rf "${dest}" && mkdir -p "${dest}"
+rm -R "${dest}" && sudo -u ubuntu mkdir -p "${dest}"
 
-# download iso19139 site records from api
-curl -s https://deims.org/api/sites | for code in $(jq .[].id.suffix | sed "s/\"//g"); do curl -s "https://deims.org/api/sites/${code}?format=iso19139" -o "${dest}/${code}"_site.xml; done
+# Download each site ISO19139 record
+curl -s https://deims.org/api/sites \
+  | jq -r '.[].id.suffix' \
+  | while read -r code; do
+      curl -s "https://deims.org/api/sites/${code}?format=iso19139" \
+        -o "${dest}/${code}.xml"
+    done
 
-# check firewall issues
+# download iso19139 from deims api manually:
+# curl -s https://deims.org/api/sites | jq -r '.[].id.suffix' | xargs -I{} -P4 curl -s "https://deims.org/api/sites/{}?format=iso19139" -o "/home/ubuntu/csw>
 # bash script might have to be adjusted for unix using $ dos2unix pycsw.sh
 
-# Find all files containing "service not found"
-# grep -Flr "Internal Server Error" /home/ubuntu/cswdatabase/iso19139_files/
+# Find and delete all files that contain different error messages
+# improved solution: just look for really small files:
+# find "${dest}" -type f -size -1000c -delete
 
-# Find and delete all files containing "Internal Server Error"
-grep -Flr "Internal Server Error" /home/ubuntu/cswdatabase/iso19139_files/  | xargs rm -f
+# grep -Flr "Internal Server Error" "${dest}"  | xargs rm -f
+# grep -Flr "value is not a valid integer" "${dest}"  | xargs rm -f
+# grep -Flr "none is not an allowed value" "${dest}"  | xargs rm -f
+# grep -Flr "value is not a valid uuid" "${dest}"  | xargs rm -f
+# grep -Flr "Proxy Error" "${dest}" | xargs rm -f
 
-# clear existing metadata records 
-docker exec -ti pycsw rm -rf /metadata/*
-docker exec -ti pycsw pycsw-admin.py delete-records -y -c /etc/pycsw/pycsw.yml 
+# clear existing metadata records
+# for executing commands in cli add -ti flag
+docker exec pycsw rm -rf /metadata/*
+docker exec pycsw pycsw-admin.py delete-records -y -c /etc/pycsw/pycsw.yml
 
-# import files to db docker container and then database
-docker cp /home/ubuntu/cswdatabase/iso19139_files pycsw:/metadata
-docker exec -ti pycsw pycsw-admin.py load-records -c /etc/pycsw/pycsw.yml -p /metadata -y -r
+
+# import files to pycsw docker container folder and then database
+docker cp "${dest}/." pycsw:/metadata
+# do it manually:
+# docker cp /home/ubuntu/cswdatabase/iso19139_files/.  pycsw:/metadata
+docker exec pycsw pycsw-admin.py load-records -c /etc/pycsw/pycsw.yml -p /metadata -y -r
